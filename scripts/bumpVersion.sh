cd3ddd72fe5e625a9bc5d41720a76180aad7389c
Martin Walter <martin.walter@tretton37.com>
2018-10-01 15:38:38 +0200

    Changing to minor instead of patch

--- scripts/bumpVersion.sh
+++ scripts/bumpVersion.sh
@@ -7,7 +7,7 @@ PATCH=$(grep -i 'PATCH' VERSION  | cut -f2 -d'=')
 echo "from: ${MAJOR}.${MINOR}.${PATCH}"
 
 # The bump
-sed -ie "/PATCH=/ s/=.*/=$((PATCH+1))/" "VERSION"
+sed -ie "/MINOR=/ s/=.*/=$((MINOR+1))/" "VERSION"
 
 MAJOR=$(grep -i 'MAJOR' VERSION  | cut -f2 -d'=')
 MINOR=$(grep -i 'MINOR' VERSION  | cut -f2 -d'=')
