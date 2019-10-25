4cac4b85d67f484423842d87d805c20485662a51
Palka Anshwika <EXT_PNS@qlik.com>
2019-10-25 11:52:56 +0530

    checking the css added

--- src/statisticBlock.js
+++ src/statisticBlock.js
@@ -425,7 +425,7 @@ class StatisticBlock extends Component {
 
     return (
       <InlineCSS namespace={`css-${qId}`} stylesheet={styles} style={{ height: "100%" }}>
-        <div className={`qv-object-qsstatistic ${this.props.services.State.isInEditMode() ? 'edit-mode' : ''}`} style={objectStyle}>
+        <div className={`qv-object-qsstatistic ${this.props.services.State.isInEditMode() ? 'edit-mode' : ''}` + "huge"} style={objectStyle}>
           {items}
         </div>
       </InlineCSS>
